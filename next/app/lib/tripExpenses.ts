8d9c2898158b543ff32f5f81e84aa43080f78f93
taunhealy <taunhealy@gmail.com>
2025-01-28 15:36:04 +0200

    vercel deploy

--- next/app/lib/tripExpenses.ts
+++ next/app/lib/tripExpenses.ts
@@ -12,34 +12,3 @@ export interface DailyExpenses {
   activities: number;
   medical: number;
 }
-
-export interface TripExpenses {
-  flights: FlightCost[];
-  accommodation: {
-    costPerNight: number;
-    numberOfNights: number;
-  };
-  dailyExpenses: DailyExpenses;
-  totalCost: number;
-}
-
-export function calculateTotalTripCost(
-  expenses: Omit<TripExpenses, "totalCost">
-): TripExpenses {
-  const flightsCost = expenses.flights.reduce(
-    (total, flight) => total + flight.price,
-    0
-  );
-  const accommodationCost =
-    expenses.accommodation.costPerNight * expenses.accommodation.numberOfNights;
-  const dailyCosts = Object.values(expenses.dailyExpenses).reduce(
-    (sum, cost) => sum + cost,
-    0
-  );
-  const totalDailyCosts = dailyCosts * expenses.accommodation.numberOfNights;
-
-  return {
-    ...expenses,
-    totalCost: flightsCost + accommodationCost + totalDailyCosts,
-  };
-}

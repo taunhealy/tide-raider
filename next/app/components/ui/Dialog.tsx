969c9e23884b886fc856e681e38e23d32bb53206
taunhealy <taunhealy@gmail.com>
2025-03-12 18:11:03 +0200

    Dialog fix

--- next/app/components/ui/Dialog.tsx
+++ next/app/components/ui/Dialog.tsx
@@ -100,7 +100,7 @@ const DialogContent = React.forwardRef<
     <dialog
       ref={dialogRef}
       className={cn(
-        "relative flex flex-col z-50 grid w-full max-w-[95vw] md:max-w-3xl gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg backdrop:bg-black/80 font-primary",
+        "relative flex-col z-50 grid w-full max-w-[95vw] md:max-w-3xl gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg backdrop:bg-black/80 font-primary",
         className
       )}
       onClick={(e) => {
